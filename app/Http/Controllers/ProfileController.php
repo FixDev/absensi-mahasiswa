2dbb86e9e4f2c21b2316a93641c215a7a57ea069
Muhammad Fikri <fikrimuhammad2016@gmail.com>
2022-06-18 18:01:49 +0700

    add profle

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -13,7 +14,8 @@ class ProfileController extends Controller
      */
     public function index()
     {
-        return view('profile.index');
+        $data = User::with('mahasiswa')->get()->first();
+        return view('profile.index')->with('user', $data);
     }
 
     /**
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -4,7 +4,29 @@
 
 @section('content')
 
-<h3>Profile</h3>
+<div class="page-header min-height-300 border-radius-xl" style="background-image: url('https://images.unsplash.com/photo-1531512073830-ba890ca4eba2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80');">
+    <span class="mask  bg-gradient-primary  opacity-6"></span>
+</div>
+<div class="card card-body mx-3 mx-md-4 mt-n6">
+    <div class="row gx-4 mb-2">
+        <div class="col-auto">
+            <div class="avatar avatar-xl position-relative">
+                <img src="{{url('/')}}/assets/img/bruce-mars.jpg" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
+            </div>
+        </div>
+        <div class="col-auto my-auto">
+            <div class="h-100">
+                <h5 class="mb-1">
+                   {{$user->mahasiswa[0]->nama}}  ({{$user->username}})
+                </h5>
+                <p class="mb-0 font-weight-normal text-sm">
+                   {{$user->email}}
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
     // Add active class to the current button (highlight it)
     var current = document.getElementsByClassName("nav-link text-white");
@@ -12,4 +34,4 @@
     current[4].className += " active bg-gradient-primary"
 </script>
 
-@endsection 
+@endsection
